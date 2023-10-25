e6dcb8be2338321cbad1f77eee09f6eb746d95b7
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-10-25 15:20:34 -0700

    phase 3

--- README.md
+++ README.md
@@ -11,22 +11,25 @@ RESTy is a React-based API testing application. In its initial phase, the app pr
 
 ## Phase 2: User Input and State
 
-- Convert <App /> to functional
+- Convert App to functional
 - Implement useState()
 - Capture user input
 - Test UI and state
 
+## Phase 3: API Integration
+
+- Connect to live APIs
+- Service GET requests
+- Display response data
+  
 ## URLs
 
 - ![UML](./public/RestyUML.png)
 - [PR 1](https://github.com/KatKho/resty/pull/1)
 - [PR 2](https://github.com/KatKho/resty/pull/3)
-
-
+- [PR 3](https://github.com/KatKho/resty/pull/4)
 
 ## Contributors
 
 - Ekaterina Khoroshilova
-
 - ChatGPT has been utilized for tests
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.scss';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -11,26 +12,43 @@ function App() {
   const [requestParams, setRequestParams] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const callApi = (requestParams) => {
-    setLoading(true)
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    setData(data);
-    setRequestParams(requestParams);
+  const callApi = async (requestParams) => {
+    setLoading(true);
+    try {
+      let config = {
+        url: requestParams.url,
+        method: requestParams.method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      
+      if (['POST', 'PUT'].includes(requestParams.method)) {
+        config.data = JSON.parse(requestParams.data || '{}');
+      }
+
+      const response = await axios(config);
+
+      setData(response.data);
+      setRequestParams(requestParams);
+    } catch (error) {
+      console.error('There was a problem with the Axios request:', error);
+    }
     setLoading(false);
-  }
+  };
+
+  useEffect(() => {
+    if (requestParams.url && requestParams.method) {
+      callApi(requestParams);
+    }
+  }, [requestParams]);
+  
     return (
       <React.Fragment>
         <Header />
         <div data-testid="request-method">Request Method: {requestParams.method}</div>
         <div>URL: {requestParams.url}</div>
-        <Form  handleApiCall={callApi} />
+        <Form handleApiCall={setRequestParams} />
         <Results data={data} loading={loading} />
         <Footer  data-testid="footer"/>
       </React.Fragment>
